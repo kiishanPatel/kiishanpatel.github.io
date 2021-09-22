8d725f83051a2bbe829db0bbce59596d549573ea
KishanPatel <89639954+kiishanPatel@users.noreply.github.com>
2021-09-22 21:58:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+
+
+
+const menuButton = document.querySelector("nav .desktop-nav .link-menu");
+const closeButton = document.querySelector(".menu-container .link-close");
+const desktopNav = document.querySelector(".desktop-nav");
+const overlay = document.querySelector(".overlay");
+
+closeButton.addEventListener("click", () => {
+    desktopNav.classList.remove("hide");
+    searchContainer.classList.add("hide");
+    overlay.classList.remove("show");
+})
+
+overlay.addEventListener("click", () => {
+    desktopNav.classList.remove("hide");
+    searchContainer.classList.add("hide");
+    overlay.classList.remove("show");
+})
+
+
+// Mobile Version
+
+const menuIconContainer = document.querySelector("nav .menu-icon-container");
+const navContainer = document.querySelector(".nav-container");
+
+menuIconContainer.addEventListener("click", () => {
+    navContainer.classList.toggle("active");
+})
+
